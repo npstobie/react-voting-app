b2510ffeb23204c20e73be3ac692b5582733975f
Nicholas Stobie <npstobie@gmail.com>
2016-07-01 13:36:29 -0700

    implemented sockets, basic server working

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 import makeStore from './src/store';
+import startServer from './src/server.js';
 
 export const store = makeStore();
+startServer();
+
+store.dispatch({
+	type: 'SET_ENTRIES',
+	entries: require('./entries.json')
+});
+store.dispatch({type: 'NEXT'});
